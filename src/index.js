d2cbd899f50a6aeb045144fa0391b5bc96d99820
Matheus <mathieus_n.7@hotmail.com>
2024-02-17 21:33:39 -0300

    Basic structure to check if we can access Weather API without problems
    For a while, we will only be displaying the temperature,
    and it's only in Celsius.
    
    The user can enter a specific location (city, state, country).
    
    I'm aware that API keys shouldn't be exposed like I'm doing
    here. But, this project is part of a course, and only later
    during backend lessons I'll learn ways to securely deal with
    these topics.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import "./style.css";
 
 console.log("working");
+
+const warning = document.createElement("p");
+warning.textContent = "Error =/ Try again or try a new key word.";
+
+const showTemp = document.querySelector(".showTemp p");
+
+getTemp("salvador");
+
+const form = document.querySelector("form");
+
+const search = document.querySelector("#search");
+
+form.addEventListener("submit", (e) => {
+  if (warning) {
+    warning.remove();
+  }
+
+  getTemp(search.value);
+
+  e.preventDefault();
+});
+
+async function getTemp(element) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=47932d30445e427ba7611035241102&q=${element}`,
+      { mode: "cors" },
+    );
+
+    const weatherData = await response.json();
+
+    showTemp.textContent = weatherData.current.temp_c;
+  } catch (err) {
+    showTemp.textContent = "";
+
+    document.body.prepend(warning);
+  }
+}
